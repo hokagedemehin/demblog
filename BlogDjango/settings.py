644b6c9b271d2226eeb762c14243b0492449e574
Demehin Ibukun <ibk2k7@gmail.com>
2020-05-31 04:17:39 +0100

    v3

--- BlogDjango/settings.py
+++ BlogDjango/settings.py
@@ -160,21 +160,21 @@ EMAIL_HOST_USER = os.environ.get('DB_EMAIL')
 
 EMAIL_HOST_PASSWORD = os.environ.get('DB_PASS')
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-AWS_S3_FILE_OVERWRITE = False
+# AWS_S3_FILE_OVERWRITE = False
 
-AWS_DEFAULT_ACL = None
+# AWS_DEFAULT_ACL = None
 
 # AWS_BUCKET_ACL = None
 
 # import boto3
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
