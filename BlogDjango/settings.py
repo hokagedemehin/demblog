9bbfbccce79a168ace642e2c32f9077d700d0c3b
Demehin Ibukun <ibk2k7@gmail.com>
2020-06-03 13:15:15 +0100

    u3

--- BlogDjango/settings.py
+++ BlogDjango/settings.py
@@ -169,7 +169,9 @@ EMAIL_HOST_PASSWORD = os.environ.get('DB_PASS')
 AWS_ACCESS_KEY_ID = 'AKIAWZBUTVX5QK4PZM4Z'
 
 # AWS_SECRET_ACCESS_KEY = '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'
-AWS_SECRET_ACCESS_KEY = os.path.join(BASE_DIR, 'key.txt')
+
+with open(os.path.join(BASE_DIR, 'key.txt')) as f:
+    AWS_SECRET_ACCESS_KEY = f.read().strip()
 
 AWS_STORAGE_BUCKET_NAME = 'django-bucket-ibk'
 
